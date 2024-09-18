3e66ed20d99e94a601242537303af811b5b6889c
Devendra Sahu <110284377+DevendraKumarSahu09@users.noreply.github.com>
2024-09-18 22:54:44 +0530

    Update Navbar.jsx
    url changed

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -23,7 +23,8 @@ const UserProfileDropdown = () => {
     setUser(null);
     try {
       const response = await axios.get("/auth/logout");
-      window.location.href = "http://localhost:5173/login";
+      //window.location.href = "http://localhost:5173/login";
+      window.location.href = "https://vidyoday-client.vercel.app/login";
     } catch (error) {
       console.log(error);
       if (error?.response?.data?.message) {
